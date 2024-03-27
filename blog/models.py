4d722ab7aa5100966de4802c0e44bb3545894341
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-03-27 08:50:03 +0500

    first commit

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category_name
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    body = models.TextField(max_length=200)
-    author = models.CharField(max_length=200)
-    create_time = models.CharField(max_length=200)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
 
 

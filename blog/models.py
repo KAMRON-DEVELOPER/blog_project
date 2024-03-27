c2e8c6d4b0d5082d3ce3565fe47e805a52105bda
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-03-27 07:58:00 +0500

    first commit

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
-# class Blog(models.Model):
-#     title = models.CharField(max_length=200)
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField(max_length=200)
+    author = models.CharField(max_length=200)
+    create_time = models.CharField(max_length=200)
+
 
